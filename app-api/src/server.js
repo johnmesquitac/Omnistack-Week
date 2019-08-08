5ef26731cd135a5cd24b31fb555ac50ea90c815d
johnmesquitac <mesquitajoaoc@outlook.com>
2019-08-07 23:00:31 -0300

    first class

---
+++ app-api/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const server = express();
+
+mongoose.connect('mongodb+srv://AKIRAuser_admin:password@akiracluster-bvd0r.mongodb.net/Omnistack?retryWrites=true&w=majority', { useNewUrlParser:true});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(5000);
